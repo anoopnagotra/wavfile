924bbe4fa5f0824109932fef6fb29733c6589b5e
ankit <ankitvarshney19@gmail.com>
2018-09-12 01:04:44 +0530

    file

--- account/urls.py
+++ account/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     url(r'^dashboard/', views.dashboard, name='dashboard'),
     url(r'^login/', views.userLogin, name='userLogin'),
     url(r'^fileupload/', views.fileUpload, name='fileUpload'),
+    # url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^logout/$', views.logout_view, name='logout_view'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
